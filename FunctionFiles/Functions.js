9605fe9e23196050526c5071cd4f2e772ed3dc9f
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-01-25 19:12:56 -0700

    up

--- FunctionFiles/Functions.js
+++ FunctionFiles/Functions.js
@@ -7,6 +7,14 @@ function addDefaultMsg(event) {
   setHTMLToBody("<b>This is the test agenda text</b><br/>", "blue-icon-16", event);
 }
 
+function addP2PMsg(event) {
+  setHTMLToBody("<b><i>Meeting Objective</i></b><br/><br/>\
+    The objective of this session is for our team to gather a solid understanding of your AP processes from vendor creation\
+    , vendor bills and associated approvals, vendor payments and advanced electronic payments and expense reporting\
+    <br/><br/><b><i>Meeting Agenda</i></b><br/><br/>\
+    <ul><li>Vendor Master</li><li>Employee Master</li><li>Vendor Bills</li></ul>", "blue-icon-16", event);
+}
+
 
 function setHTMLToBody(text, icon, event) {
   Office.context.mailbox.item.body.setSelectedDataAsync(text, { coercionType: Office.CoercionType.Html }, 
