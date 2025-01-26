eadf3c7c4b790a48ba4011c3eb0e907915c88d00
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-01-25 19:19:47 -0700

    up

--- FunctionFiles/Functions.js
+++ FunctionFiles/Functions.js
@@ -11,7 +11,7 @@ function addP2PMsg(event) {
   setHTMLToBody("<b><i>Meeting Objective</i></b><br/><br/>\
     The objective of this session is for our team to gather a solid understanding of your AP processes from vendor creation\
     , vendor bills and associated approvals, vendor payments and advanced electronic payments and expense reporting\
-    <br/><br/><b><i>Meeting Agenda</i></b><br/><br/>\
+    <br/><br/><br/><b><i>Meeting Agenda</i></b><br/>\
     <ul><li>Vendor Master</li><li>Employee Master</li><li>Vendor Bills</li></ul>", "blue-icon-16", event);
 }
 
