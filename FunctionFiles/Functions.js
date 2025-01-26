824e3d169c8a0927d1ae751cdbd23e30f847637b
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-01-25 19:44:30 -0700

    up

--- FunctionFiles/Functions.js
+++ FunctionFiles/Functions.js
@@ -4,7 +4,8 @@ Office.initialize = function () {
 }
 
 function addDefaultMsg(event) {
-  setHTMLToBody("<b>This is the test agenda text</b><br/>", "blue-icon-16", event);
+  setHTMLToSubject("Default Subject Line", "blue-icon-16", event);
+  setHTMLToBody("<b>This is the default agenda text</b><br/>", "blue-icon-16", event);
 }
 
 function addP2PMsg(event) {
@@ -20,6 +21,28 @@ function addP2PMsg(event) {
     ", "blue-icon-16", event);
 }
 
+
+function setHTMLToSubject(text, icon, event) {
+  Office.context.mailbox.item.subject.setAsync(text, 
+    function (asyncResult){
+      if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+        Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+          type: "informationalMessage",
+          icon: icon,
+          message: "Success",
+          persistent: false
+        });
+      }
+      else {
+        Office.context.mailbox.item.notificationMessages.addAsync("error", {
+          type: "errorMessage",
+          message: "Failed - " + asyncResult.error.message,
+          persistent: false
+        });
+      }
+      event.completed();
+    });
+} 
 
 function setHTMLToBody(text, icon, event) {
   Office.context.mailbox.item.body.setSelectedDataAsync(text, { coercionType: Office.CoercionType.Html }, 
