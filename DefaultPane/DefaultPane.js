fb448127c3abcb9b3a362dd8f3a409f3e979f8f7
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-01-27 18:32:14 -0700

    UP

---
+++ DefaultPane/DefaultPane.js
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+
+/// <reference path="../App.js" />
+
+let _settings;
+
+(function () {
+    "use strict";
+  
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+            
+            _settings = Office.context.roamingSettings;
+  
+            $('#save').click(saveAgenda);
+        });
+    };
+    
+    function saveAgenda(event){
+
+        var text = $('#subjectToSave').val();
+        _settings.set("subject", text);
+    }
+    
+})();
