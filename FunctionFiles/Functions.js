5d9b4e85fe681ab4afc57c8c0f141e497d0f9b7c
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-01-28 18:07:48 -0700

    up

--- FunctionFiles/Functions.js
+++ FunctionFiles/Functions.js
@@ -1,75 +1,92 @@
 
+/// <reference path="../App.js" />
+
 
 let _mailbox;
 let _settings;
 
-Office.initialize = function () {
+/* Office.initialize = function () {
   _mailbox = Office.context.mailbox;
   _settings = Office.context.roamingSettings;
-}
+} */
 
-function insertDefaultAgenda(event) {
 
-    var subject = _settings.get("subject");
-    console.log(subject + " - sub7")
+(function () {
+    "use strict";
 
-    setTextToSubject(subject);
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
 
-    //var body = _settings.get("body");
-    //console.log(body + " - body7");
+            $('#insertText').click(insertText);
+        });
+    };)
 
-    //await setHTMLToBody(body, event);
+    function insertDefaultAgenda(event) {
 
-    event.completed();
-}
-  
-  
-  
-function setTextToSubject(text) {
+        var subject = _settings.get("subject");
+        console.log(subject + " - sub7")
 
-    _mailbox.item.subject.setAsync(text, 
+        setTextToSubject(subject);
 
-        function (asyncResult){
-            // Display the result to the user
-            if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-                _mailbox.item.notificationMessages.replaceAsync("status", {
-                type: "informationalMessage",
-                icon: "icon-16",
-                message: "Save successful",
-                persistent: false
-                });        
+        //var body = _settings.get("body");
+        //console.log(body + " - body7");
+
+        //await setHTMLToBody(body, event);
+
+        event.completed();
+    }
+    
+    
+    
+    function setTextToSubject(text) {
+
+        _mailbox.item.subject.setAsync(text, 
+
+            function (asyncResult){
+                // Display the result to the user
+                if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+                    _mailbox.item.notificationMessages.replaceAsync("status", {
+                    type: "informationalMessage",
+                    icon: "icon-16",
+                    message: "Save successful",
+                    persistent: false
+                    });        
+                }
+                else {
+                    _mailbox.item.notificationMessages.replaceAsync("error", {
+                    type: "errorMessage",
+                    message: "Save Failed - " + asyncResult.error.message,
+                    persistent: false
+                    }); 
+                }    
             }
-            else {
-                _mailbox.item.notificationMessages.replaceAsync("error", {
-                type: "errorMessage",
-                message: "Save Failed - " + asyncResult.error.message,
-                persistent: false
-                }); 
-            }    
-        }
-    ); 
-} 
-  
-function setHTMLToBody(html, event) {
-    _mailbox.item.body.setSelectedDataAsync(html, { coercionType: Office.CoercionType.Html }, 
+        ); 
+    } 
+    
+    function setHTMLToBody(html, event) {
+        _mailbox.item.body.setSelectedDataAsync(html, { coercionType: Office.CoercionType.Html }, 
 
-        function (asyncResult){
-            // Display the result to the user
-            if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-                _mailbox.item.notificationMessages.replaceAsync("status", {
-                type: "informationalMessage",
-                icon: "icon-16",
-                message: "Save successful",
-                persistent: false
-                });        
+            function (asyncResult){
+                // Display the result to the user
+                if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+                    _mailbox.item.notificationMessages.replaceAsync("status", {
+                    type: "informationalMessage",
+                    icon: "icon-16",
+                    message: "Save successful",
+                    persistent: false
+                    });        
+                }
+                else {
+                    _mailbox.item.notificationMessages.replaceAsync("error", {
+                    type: "errorMessage",
+                    message: "Save Failed - " + asyncResult.error.message,
+                    persistent: false
+                    }); 
+                }   
             }
-            else {
-                _mailbox.item.notificationMessages.replaceAsync("error", {
-                type: "errorMessage",
-                message: "Save Failed - " + asyncResult.error.message,
-                persistent: false
-                }); 
-            }   
-        }
-    );
-} 
+        );
+    } 
+
+})();
