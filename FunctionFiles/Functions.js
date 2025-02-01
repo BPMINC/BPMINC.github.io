df9df880aaa48b0c71fc8631afce361b1060e407
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-01-31 21:29:52 -0700

    up

--- FunctionFiles/Functions.js
+++ FunctionFiles/Functions.js
@@ -13,33 +13,40 @@ function insertDefaultAgenda(event) {
     var subject = _settings.get("subject")
     console.log(subject + " - sub1")
 
-    setTextToSubject(subject);
+    setTextToSubject(subject, event);
 
     //var body = _settings.get("body")
     //console.log(body + " - body7");
 
     //await setHTMLToBody(body, event);
 
-    event.completed();
+   //event.completed();
 }
     
 
-function setTextToSubject(text) {
+function setTextToSubject(text, event) {
 
-    _mailbox.item.body.setSelectedDataAsync(
-        text,
-        { coercionType: Office.CoercionType.Html },
-        function (asyncResult) {
-            myLogger("finished check");
-        }
-    ); 
-
-    console.log("finish function")
+    Office.context.mailbox.item.body.setSelectedDataAsync(text, { coercionType: Office.CoercionType.Text }, 
+        function (asyncResult){
+          if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+            statusUpdate(icon, "inserted successfully.");
+          }
+          else {
+            Office.context.mailbox.item.notificationMessages.addAsync("addTextError", {
+              type: "errorMessage",
+              message: "Failed to insert " + asyncResult.error.message
+            });
+          }
+          event.completed();
+        });
 }
 
 
-function myLogger(message){
-    return function(){
-        console.log(message);
-    }      
-}
+function statusUpdate(icon, text) {
+    Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+      type: "informationalMessage",
+      icon: icon,
+      message: text,
+      persistent: false
+    });
+  }
