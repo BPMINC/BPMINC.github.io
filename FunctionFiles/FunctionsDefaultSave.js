60bae584ffbbabd721a24cecae223b69eef77817
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-02-01 17:34:40 -0700

    up

--- FunctionFiles/FunctionsDefaultSave.js
+++ FunctionFiles/FunctionsDefaultSave.js
@@ -1,10 +1,53 @@
 
 async function saveDefaultAgenda(event){
 
-    let result = await getBody();
-    await setBody(result);
+    let subject = await getSubject();
+    await setSubject(subject);
+
+    let body = await getBody();
+    await setBody(body);
 
 }
+
+function getSubject(){
+
+    return new Promise(function (resolve, reject) {
+
+        try {
+            Office.context.mailbox.item.subject.getAsync(
+                Office.CoercionType.Text, 
+                function (result) {
+                    if (result.status === Office.AsyncResultStatus.Succeeded) {
+                        resolve(result.value);
+                    } else {
+                        reject(result.error);
+                    }
+                }
+            );
+        } catch {
+            reject("Unable to get email subject text.");
+        }
+    })
+}
+
+
+function setSubject(body){
+
+    return new Promise(function (resolve, reject) {
+           
+        try{
+    
+            _settings.set("body", body);
+            _settings.saveAsync();
+
+            resolve();
+        }
+        catch (error){
+              reject();
+        }
+    })
+}
+
 
 function getBody(){
 
