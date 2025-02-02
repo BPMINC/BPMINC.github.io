07b0324cf9fc48d963bc4e624c127e3dd7eecdbd
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-02-01 19:29:11 -0700

    up

---
+++ ReqsAgenda/ReqsR2RInsert.js
@@ -0,0 +1,65 @@
+
+async function insertReqsR2RAgenda(event) {
+
+    var subject = getTextForSubject();
+    await setTextToSubject(subject);
+  
+    //var body = _settings.get("body")
+    //await setHTMLToBody(body);
+  
+    event.completed();
+  }
+
+  function getTextForSubject(){
+
+    return "<b><i>Meeting Objective</i></b><br/><br/>\
+    The objective of this session is for our team to gather \
+    a solid understanding of your AP processes from vendor \
+    creation, vendor bills and associated approvals, vendor \
+    payments and advanced electronic payments and expense reporting\
+    <br/><br/><br/><b><i>Meeting Agenda</i></b><br/>\
+    <ul><li>Vendor Master</li><li>Employee Master</li>\
+    <li>Vendor Bills</li><li>Vendor Payments</li>\
+    <li>Expense Reports</li><li>Fixed Assets</li></ul>\
+    "
+
+  }
+  
+  function setTextToSubject(text) {
+  
+    return new Promise(function (resolve, reject) {
+      try{
+  
+        _mailbox.item.subject.setAsync(
+          text,         
+          function (asyncResult){
+            statusUpdate(asyncResult,"Insert");
+            resolve();
+          }
+        );
+      }
+      catch (error){
+            reject();
+      }
+    })
+  }
+  
+  function setHTMLToBody(html) {
+  
+    return new Promise(function (resolve, reject) {
+      try{
+  
+        _mailbox.item.body.setSelectedDataAsync(
+          html, 
+          { coercionType: Office.CoercionType.Html }, 
+          function (asyncResult){
+            statusUpdate(asyncResult,"Insert");
+            resolve();
+          }
+        );
+      }
+      catch (error){
+            reject();
+      }
+    })
+  }
