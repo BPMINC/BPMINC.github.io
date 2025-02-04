673366f3b448ebb85767933cc6091d3533d886e2
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-02-04 12:51:47 -0700

    up

--- ReqsAgenda/ReqsR2RInsert.js
+++ ReqsAgenda/ReqsR2RInsert.js
@@ -38,7 +38,7 @@ async function insertReqsR2RAgenda(event) {
   function getHTMLForBody(){
 
     return "Hi Team,<br /><br />Please join us to review the Record To Report \
-    business requirements.<b><i>Meeting Objective</i></b><br/><br/>\
+    business requirements.<br /><b><i>Meeting Objective</i></b><br/><br/>\
     The objective of this session is for our team to gather \
     a solid understanding of your AP processes from vendor \
     creation, vendor bills and associated approvals, vendor \
