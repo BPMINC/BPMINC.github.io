88599454ca47a0584b12fdff2c9435d724538284
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-07-01 04:23:42 +0000

    various

---
+++ SettingsPane/SettingsPane.js
@@ -0,0 +1,10 @@
+const docxTables = require('docx-tables');
+
+docxTables({ file: 'c:/temp/simple.docx' })
+  .then((data) => {
+    // 'data' will contain the extracted table data in JSON format
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
