90bae4d02cb5404fbaff76100217fda27ff06b76
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-07-01 05:15:23 +0000

    .

--- SettingsPane/SettingsPane.js
+++ SettingsPane/SettingsPane.js
@@ -9,10 +9,18 @@
 //     console.error(error);
 //   });
 
-import {fromEvent} from 'file-selector';
 
-// Open file picker
-const handles = await window.showOpenFilePicker({multiple: true});
-// Get the files
-const files = await fromEvent(handles);
-console.log(files);
+
+
+// import {fromEvent} from 'file-selector';
+
+// // Open file picker
+// const handles = await window.showOpenFilePicker({multiple: true});
+// // Get the files
+// const files = await fromEvent(handles);
+// console.log(files);
+
+import * as fs from 'node:fs';
+
+let filePath = "/home/mysystem/dev/myproject/sayHello.txt";
+let newFile = fs.readFileSync(filePath);
