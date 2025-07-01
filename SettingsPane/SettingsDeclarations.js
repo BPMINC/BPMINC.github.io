a105fd49e6e2ad1a3e58b06ba857877348f91ae0
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-07-01 21:45:28 +0000

    .

--- SettingsPane/SettingsDeclarations.js
+++ SettingsPane/SettingsDeclarations.js
@@ -30,110 +30,86 @@ function setBillingRates(rate) {
   runningTotalHours = 0;
   runningTotalAmount = 0;
 
-  // var xhr = [
-  //   setMeetingsRate(rate),
-  //   setMeetingsRate(rate),
-  //   setMeetingsRate(rate)
-  // ];
+  var xhr = [
+    setMeetingsRate(rate),
+    setMeetingsRate(rate),
+    setMeetingsRate(rate)
+  ];
 
-  // $.when(xhr[0], xhr[1], xhr[2]).then(function () {
-  //   showGrandTotal();
-  // });
+  $.when(xhr[0], xhr[1], xhr[2]).then(function () {
+    //showGrandTotal();
+  });
 }
 
 
-// // Constructs the meetings table and calculated the total
-// // billing amount for each item and for all meetings.
-// function setMeetingsRate(rate) {
-//     return $.getJSON("../../assets/sampleMeetingData.json", function (data) {
-//         var jsonData = data.Appointments;
+// Constructs the meetings table and calculated the total
+// billing amount for each item and for all meetings.
+function setMeetingsRate(rate) {
+    return $.getJSON("../assets/customerList.json", function (data) {
+        var jsonData = data.Customers;
 
-//         var dataTable = $("#app-Meetings-table");
-//         dataTable.html("");
+        var dataTable = $("#app-Meetings-table");
+        dataTable.html("");
 
-//         var headerRow = $('<div />');
-//         headerRow.append(makeHeaderCell("Subject", "5"));
-//         headerRow.append(makeHeaderCell("Attendees", "5"));
-//         headerRow.append(makeHeaderCell("Hours", "1", "true"));
-//         headerRow.append(makeHeaderCell("Total", "1", "true"));
+        var headerRow = $('<div />');
+        headerRow.append(makeHeaderCell("Name", "5"));
+        headerRow.append(makeHeaderCell("RMID", "5"));
+        headerRow.append(makeHeaderCell("SOW", "1", "true"));
 
-//         dataTable.append(headerRow);
+        dataTable.append(headerRow);
 
-//         var totalHours = 0;
-//         var totalAmount = 0;
+        var totalHours = 0;
+        var totalAmount = 0;
 
-//         for (var i in jsonData) {
-//             var dataRow = $("<div />", {
-//                 "class": "ms-Grid-row app-Grid-row"
-//             });
-//             dataRow.append(makeRowCell(jsonData[i].Subject, "5"));
-//             dataRow.append(makeRowCell(jsonData[i].Attendees, "5"));
-//             dataRow.append(makeRowCell(jsonData[i].Hours, "1", "true"));
-//             dataRow.append(makeRowCell(jsonData[i].Hours * rate, "1", "true"));
+        for (var i in jsonData) {
+            var dataRow = $("<div />", {
+                "class": "ms-Grid-row app-Grid-row"
+            });
+            dataRow.append(makeRowCell(jsonData[i].Name, "5"));
+            dataRow.append(makeRowCell(jsonData[i].RMID, "5"));
+            dataRow.append(makeRowCell(jsonData[i].SOW_Path, "1", "true"));
 
-//             totalHours += Number(jsonData[i].Hours);
-//             totalAmount += rate * (jsonData[i].Hours);
+            totalHours += Number(jsonData[i].Hours);
+            totalAmount += rate * (jsonData[i].Hours);
 
-//             dataTable.append(dataRow);
-//         }
+            dataTable.append(dataRow);
+        }
         
-//         dataTable.append(makeTotalRow(totalHours, totalAmount));
 
-//         runningTotalHours += totalHours;
-//         runningTotalAmount += totalAmount;
-//     });
-// };
+        runningTotalHours += totalHours;
+        runningTotalAmount += totalAmount;
+    });
+};
 
 
-// function makeHeaderCell(text, width, right) {
-//     var cssClass = "ms-Grid-col ms-fontColor-themeDark ms-font-l ms-u-lg" + width;
+function makeHeaderCell(text, width, right) {
+    var cssClass = "ms-Grid-col ms-fontColor-themeDark ms-font-l ms-u-lg" + width;
 
-//     if (right) {
-//         cssClass += " app-Cell-right";
-//     }
+    if (right) {
+        cssClass += " app-Cell-right";
+    }
 
-//     return $("<div />", {
-//         "class": cssClass,
-//         "html": text
-//     })
-// };
+    return $("<div />", {
+        "class": cssClass,
+        "html": text
+    })
+};
 
-// // Creates the HTML for displaying a table cell.
-// function makeRowCell(text, width, right) {
-//     var cssClass = "ms-Grid-col ms-u-md4 ms-u-lg" + width;
+// Creates the HTML for displaying a table cell.
+function makeRowCell(text, width, right) {
+    var cssClass = "ms-Grid-col ms-u-md4 ms-u-lg" + width;
 
-//     if (right) {
-//         cssClass += " app-Cell-right";
-//     }
+    if (right) {
+        cssClass += " app-Cell-right";
+    }
 
-// 	return $("<div />", {
-//         "class" : cssClass,
-//         "html"  : text
-//     });    
-// }
+	return $("<div />", {
+        "class" : cssClass,
+        "html"  : text
+    });    
+}
 
-// function makeTotalRow(totalHours, totalAmount) {
-//         var totalRow = $("<div />", {
-//             "class": "ms-Grid-row  ms-fontColor-themeDark ms-font-l app-Grid-row"
-//         });
 
-//         totalRow.append($("<div />", {
-//             "class": "app-Cell-right ms-Grid-col ms-u-lg10",
-//             "html": "Totals:"
-//         }));
-
-//         totalRow.append($("<div />", {
-//             "class": "app-Cell-right ms-Grid-col ms-u-lg1",
-//             "html": totalHours
-//         }));
-
-//         totalRow.append($("<div />", {
-//             "class": "app-Cell-right ms-Grid-col ms-u-lg1",
-//             "html": totalAmount
-//         }));
-
-//     return totalRow;
-// }
 
 // // Creates the row that displays the grand total for the page.
 // function showGrandTotal() {
