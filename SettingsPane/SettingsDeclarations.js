f4502632180bd614e878aa103fec249459de9d7b
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-07-01 22:34:19 +0000

    .

--- SettingsPane/SettingsDeclarations.js
+++ SettingsPane/SettingsDeclarations.js
@@ -31,15 +31,6 @@ function getCustomers() {
         var dataTable = $("#app-Customers-table");
         dataTable.html("");
 
-        var headerRow = $('<div />');
-        headerRow.append(makeHeaderCell("Name", "3"));
-        headerRow.append(makeHeaderCell("RMID", "2"));
-        headerRow.append(makeHeaderCell("SOW", "5", "true"));
-
-        dataTable.append(headerRow);
-
-
-
         for (var i in jsonData) {
             var dataRow = $("<div />", {
                 "class": "ms-Grid-row app-Grid-row"
@@ -48,32 +39,13 @@ function getCustomers() {
             dataRow.append(makeRowCell(jsonData[i].RMID, "2"));
             dataRow.append(makeRowCell(jsonData[i].SOW_Path, "5", "true"));
 
-   
-
             dataTable.append(dataRow);
         }
-        
-
-        console.log(jsonData);
 
     });
 };
 
 
-function makeHeaderCell(text, width, right) {
-    var cssClass = "ms-Grid-col ms-font-l ms-u-lg" + width;
-
-    if (right) {
-        cssClass += " app-Cell-right";
-    }
-
-    return $("<div />", {
-        "class": cssClass,
-        "html": text
-    })
-};
-
-// Creates the HTML for displaying a table cell.
 function makeRowCell(text, width, right) {
     var cssClass = "ms-Grid-col ms-u-md4 ms-u-lg" + width;
 
