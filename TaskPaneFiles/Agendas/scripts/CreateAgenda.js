5889c04e4af708e89c16cdc764aa83759c70bd7f
DigitalVision <46016928+josephsmi2013@users.noreply.github.com>
2025-07-07 05:14:46 +0000

    .

--- TaskPaneFiles/Agendas/scripts/CreateAgenda.js
+++ TaskPaneFiles/Agendas/scripts/CreateAgenda.js
@@ -201,7 +201,8 @@ function createAttendees(customer) {
 
             if (customer == jsonData[i].Name) {
 
-                var attendeeList = [{emailAddress: undefined, displayName: 'OneWeb Team', recipientType: 'distributionList'}]
+                //var attendeeList = [{emailAddress: undefined, displayName: 'OneWeb Team', recipientType: 'distributionList'}]
+                var attendeeList = [];
 
                 setTextToAttendees(attendeeList);
 
